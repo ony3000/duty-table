8f669f05ee5747e118c91f9751e56d477af39c89
Hyeonjong <nianelo4@gmail.com>
2023-07-16 17:35:35 +0900

    chore: pretendard 폰트 적용

--- app.vue
+++ app.vue
@@ -4,6 +4,17 @@ useHead({
     lang: 'ko',
   },
   title: '당직 시간표',
+  link: [
+    {
+      rel: 'preconnect',
+      href: 'https://cdn.jsdelivr.net',
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.8/dist/web/static/pretendard.css',
+      crossorigin: '',
+    },
+  ],
 });
 </script>
 
@@ -12,3 +23,12 @@ useHead({
     <AppMain />
   </ClientOnly>
 </template>
+
+<style>
+body {
+  font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto,
+    'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR',
+    'Malgun Gothic', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol',
+    sans-serif;
+}
+</style>
